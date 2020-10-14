bda6ccd9649ca287a3aa2f67cc1fdea9b646d4b8
snira <snirdavid1994@gmail.com>
2020-10-14 11:14:47 +0300

    add the function in header file

---
+++ anomaly_detection.h
@@ -0,0 +1,48 @@
+//
+// Created by snira on 14/10/2020.
+//
+
+#ifndef ADVANCE_PROGRAMING_PROJECT1_ANOMALY_DETECTION_H
+#define ADVANCE_PROGRAMING_PROJECT1_ANOMALY_DETECTION_H
+
+
+class anomaly_detection {
+// returns the variance of X and Y
+    float var(float *x, int size);
+
+// returns the covariance of X and Y
+    float cov(float *x, float *y, int size);
+
+// returns the Pearson correlation coefficient of X and Y
+    float pearson(float *x, float *y, int size);
+
+    class Line {
+    public:
+        const float a, b;
+
+        Line(float a, float b) : a(a), b(b) {}
+
+        float f(float x) {
+            return a * x + b;
+        }
+    };
+
+    class Point {
+    public:
+        const float x, y;
+
+        Point(float x, float y) : x(x), y(x) {}
+    };
+
+// performs a linear regression and return s the line equation
+    Line linear_reg(Point **points, int size);
+
+// returns the deviation between point p and the line equation of the points
+    float dev(Point p, Point **points, int size);
+
+// returns the deviation between point p and the line
+    float dev(Point p, Line l);
+};
+
+
+#endif //ADVANCE_PROGRAMING_PROJECT1_ANOMALY_DETECTION_H
---
+++ anomaly_detection_lib.cpp
@@ -0,0 +1,5 @@
+//
+// Created by snira on 14/10/2020.
+//
+
+#include "anomaly_detection.h"
