99bc40bcb05557f57827192fa1affdb8219a447b
Snir David Nahari <snirdavid1994@gmail.com>
2020-10-14 21:44:22 +0300

    changing the name to util

--- anomaly_detection_util.h
+++ anomaly_detection_util.h
@@ -1,15 +1,20 @@
-//
-// Created by snira on 14/10/2020.
-//
 
-#ifndef ADVANCE_PROGRAMING_PROJECT1_ANOMALY_DETECTION_H
-#define ADVANCE_PROGRAMING_PROJECT1_ANOMALY_DETECTION_H
+#ifndef ANOMALYDETECTORUTIL_H_
+#define ANOMALYDETECTORUTIL_H_
+
+
+float avg(float* x, int size);
+
 // returns the variance of X and Y
 float var(float* x, int size);
+
 // returns the covariance of X and Y
-float cov(float* x, float* y, int size);
+float cov(float* x,  float* y, int size);
+
+
 // returns the Pearson correlation coefficient of X and Y
 float pearson(float* x, float* y, int size);
+
 class Line{
 public:
     const float a,b;
@@ -18,15 +23,20 @@ public:
         return a*x+b;
     }
 };
+
 class Point{
 public:
     const float x,y;
-    Point(float x, float y):x(x),y(x){}
+    Point(float x, float y):x(x),y(y){}
 };
-// performs a linear regression and return s the line equation
+
+// performs a linear regression and returns the line equation
 Line linear_reg(Point** points, int size);
+
 // returns the deviation between point p and the line equation of the points
 float dev(Point p,Point** points, int size);
+
 // returns the deviation between point p and the line
 float dev(Point p,Line l);
-#endif //ADVANCE_PROGRAMING_PROJECT1_ANOMALY_DETECTION_H
+
+#endif
